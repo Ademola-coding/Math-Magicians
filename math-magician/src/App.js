28bfd265f4eeea72b7e14d9f96b3af9b32126194
ademola <ademolaowoeye07@gmail.com>
2022-09-15 05:53:23 +0100

    USED CLASS COMPONTENT TO CREATE UI

---
+++ math-magician/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calculator from './components/calculator';
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">calculator App(first project Yeah!!!)</h1>
+      <Calculator />
+    </div>
+  );
+}
+export default App;
