308e91a096647a504a6505448db720ce7bbe4aed
ademola <ademolaowoeye07@gmail.com>
2022-09-15 06:15:05 +0100

    updated

--- math-magician/src/App.js
+++ math-magician/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import React from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div>
-      <h1 className="heading">calculator App(first project Yeah!!!)</h1>
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="heading">calculator App(first project Yeah!!!)</h1>
+        <Calculator />
+      </div>
+    );
+  }
 }
 export default App;
