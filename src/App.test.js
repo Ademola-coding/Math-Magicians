45781e1a0bd31dc59e91602cb663d928032d74b1
ademola <ademolaowoeye07@gmail.com>
2022-09-24 19:41:25 +0100

    Deploying

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,39 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// snapshot automatically pops a json note file
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// describe('Testing App components', () => {
+//   test('Rendering the Header component', () => {
+//     const component = render(
+//       <BrowserRouter>
+//         <Header />
+//       </BrowserRouter>,
+//     );
+//     expect(component).toMatchSnapshot();
+//   });
+//   test('Rendering the App component', () => {
+//     const component = render(
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>,
+//     );
+//     expect(component).toMatchSnapshot();
+//   });
+//   test('Rendering the Footer component', () => {
+//     const component = render(
+//       <BrowserRouter>
+//         <Footer />
+//       </BrowserRouter>,
+//     );
+//     expect(component).toMatchSnapshot();
+//   });
+// });
